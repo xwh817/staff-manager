54cb73eb43fb71e6038a13eb3c5aa953aa41b639
shine <shine@turingcat.com>
2019-04-17 18:04:24 +0800

    add redux-thunk

--- front-end/src/redux/actions.js
+++ front-end/src/redux/actions.js
@@ -1,6 +1,34 @@
+import ApiUtil from '../Utils/ApiUtil'
+import HttpUtil from '../Utils/HttpUtil'
 /**
  * 将action抽取出来
  * action名字 => funciton调用
  */
+
+
+export const getJobs = () => (dispatch, getState) => {
+    dispatch({
+        type:'get_jobs_start',
+    });
+     //先去请求数据
+    HttpUtil.get(ApiUtil.API_JOB_LIST)
+        .then(
+            jobList => {
+                //请求数据完成后再dispatch
+                dispatch({
+                    type:'get_jobs_success',
+                    payload:jobList
+                })
+            }
+        )
+        .catch(e => {
+            console.log(e)
+            dispatch({
+                type:'get_jobs_fail',
+                payload:e
+            })
+        })
+};
+
 
 export const addJob = () => ({ type: 'addJob' });
